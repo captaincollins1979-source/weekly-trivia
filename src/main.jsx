4f2c481fb2e243238201acf48ca196f6f34d15dc
captaincollins1979-source <captaincollins1979@gmail.com>
2025-09-19 18:03:46 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import WeeklyTriviaMiniApp from "./WeeklyTrivia.jsx";
 
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <WeeklyTriviaMiniApp />
+  </React.StrictMode>
+);
